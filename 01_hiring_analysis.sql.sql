a17432178f28551b0aefeff5cb3009069e1594e4
Yatin Bahl <87776992+yatinbahl26@users.noreply.github.com>
2026-01-14 05:21:30 +0530

    Refactor SQL query for Data Analyst job postings
    Refactored SQL query to filter job postings for Data Analyst roles based on location, experience, and salary criteria.

--- 01_hiring_analysis.sql.sql
+++ 01_hiring_analysis.sql.sql
@@ -1,45 +1,22 @@
 -- 🎯 BUSINESS + INTERVIEW QUESTION #1 (LOCKED)
+
 -- 👉 Find all Data Analyst–related roles that:
-
 -- Are located in Delhi / Gurugram / Noida / NCR
+-- -- Require 0–1 year experience
+-- -- Offer at least 6 LPA maximum salary
 
-SELECT * FROM job_postings;
-
--- WITH loc AS (
--- 	SELECT role_title FROM job_postings
---     WHERE location = 'Noida'
--- )
--- SELECT * FROM loc
--- -- WHERE location = 'Noida'
-
-
--- Require 0–1 year experience
-
--- WITH exp AS (
--- SELECT role_title FROM job_postings
--- 	WHERE experience_required 
---     BETWEEN 0.0 AND 0.5
--- )
--- SELECT * FROM exp;
-
--- Offer at least 6 LPA maximum salary
-
--- WITH sal AS (
--- 	SELECT role_title FROM job_postings
---     WHERE min_salary_lpa > 6.0
--- )
--- SELECT * FROM sal;
-
--- Show company name, role, location, salary range, work mode
-
-WITH comp_details AS (
-	SELECT company_name, 
-		   role_title, 
-           location, 
-           min_salary_lpa, 
-           max_salary_lpa, 
-           work_mode 
+WITH filtered_jobs AS (
+    SELECT
+        company_name,
+        role_title,
+        location,
+        min_salary_lpa,
+        max_salary_lpa,
+        work_mode
     FROM job_postings
+    WHERE location IN ('Delhi NCR', 'Gurugram', 'Noida', 'Remote')
+      AND experience_required <= 1.0
+      AND max_salary_lpa >= 6.0
 )
-
-SELECT * FROM comp_details
+SELECT *
+FROM filtered_jobs;
