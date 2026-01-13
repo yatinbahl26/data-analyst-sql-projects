87417c1a9de18d952bdc88c12611676f38045465
Yatin Bahl <87776992+yatinbahl26@users.noreply.github.com>
2026-01-14 05:22:54 +0530

    Add query for eligible Data Analyst roles count
    Added a query to count eligible Data Analyst roles per company based on specified criteria.

--- 01_hiring_analysis.sql.sql
+++ 01_hiring_analysis.sql.sql
@@ -20,3 +20,23 @@ WITH filtered_jobs AS (
 )
 SELECT *
 FROM filtered_jobs;
+
+
+-- 👉 Show the number of eligible Data Analyst roles per company
+-- that satisfy:
+
+-- Location: Delhi / Gurugram / Noida / NCR / Remote
+-- Experience required: ≤ 1 year
+-- Max salary: ≥ 6 LPA
+-- Expected output columns:
+-- company_name | total_roles
+
+
+WITH company_roles AS (
+    SELECT company_name , COUNT(role_title) AS total_roles FROM job_postings
+    WHERE location IN ('Delhi NCR', 'Gurugram', 'Noida')
+    AND experience_required <= 1.0
+    AND max_salary_lpa >= 6.0
+    GROUP BY company_name
+)
+SELECT * FROM company_roles;
